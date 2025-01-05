bcfcf3d712a5bf961d308074b41d464994a519cb
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-01-05 22:46:32 +0530

    Add files via upload

---
+++ Basic Programs/palindrome string.py
@@ -0,0 +1,12 @@
+from numpy.random import poisson
+
+n = input("enter a string : ")
+temp = n
+reverse = ""
+while len(temp) > 0:
+    reverse += temp[-1]
+    temp = temp[:-1]
+if n == reverse:
+    print("palindrome")
+else:
+    print("Not a palindrome")
