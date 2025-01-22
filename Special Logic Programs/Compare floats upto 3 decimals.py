6bfd55c99b99a4bee0ea4bd49cef5ddb048226ca
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-01-22 23:21:11 +0530

    Add files via upload

---
+++ Special Logic Programs/Compare floats upto 3 decimals.py
@@ -0,0 +1,12 @@
+a = float(input("Enter a : "))
+b = float(input("Enter b : "))
+c = int(a*1000)/1000
+d = int(b*1000)/1000
+print(c)
+print("c = ",c)
+print("d = ",d)
+
+if c == d :
+    print("equal")
+else:
+    print("Not equal")
