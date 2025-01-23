0d8c99973c5f252df0f7ab44949459e83c22c432
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-01-23 15:50:53 +0530

    Update Leap Year.py

--- Basic Programs/Leap Year.py
+++ Basic Programs/Leap Year.py
@@ -1,6 +1,6 @@
 Year = eval(input("Enter the Year:"))
 
-if Year % 4 == 0 or Year % 100 == 0 and Year % 400 == 0:
+if Year % 4 == 0 and  Year % 100 == 0 or Year % 400 == 0:
     print("Leap Year")
 
 else:
