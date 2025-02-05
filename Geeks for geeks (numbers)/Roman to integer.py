4afc85ba8e627b214b7b74b3cb6a47c11a569c7d
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-02-05 23:37:25 +0530

    Add files via upload

---
+++ Geeks for geeks (numbers)/Roman to integer.py
@@ -0,0 +1,22 @@
+from scipy.constants import value
+
+s = input("Enter a string romans : ")
+r ={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+
+total = 0
+previous =  0
+
+for i in s[::-1]:
+    value = r[i]
+    if value < previous:
+        total -= value
+    else:
+        total += value
+    previous = value
+print(total)
+
+
+
+
+
+
