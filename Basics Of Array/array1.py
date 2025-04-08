47e2844b693cda76b07d39b6bbd541cb3bd47c59
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-04-08 18:29:55 +0530

    Update array1.py

--- Basics Of Array/array1.py
+++ Basics Of Array/array1.py
@@ -1,1 +1,29 @@
+a = [1,2,3,4]
+print(a)
 
+#Append 5
+a.append(7)
+print(a)
+
+#pop
+a.pop()
+print(a)
+
+#insert
+a.insert(3,77)
+print(a)
+
+#conditions
+if 77 in a:
+    print(True)
+else :
+    print(False)
+
+#Accesing element
+print(a[4])
+
+#modify
+a[0] = 77
+print(a)
+
+print(len(a))
