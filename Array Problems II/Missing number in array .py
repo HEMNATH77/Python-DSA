0acf280463707f36d777fea1681a0c68681e43c0
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-04-22 22:06:47 +0530

    Update Missing number in array .py

--- Array Problems II/Missing number in array .py
+++ Array Problems II/Missing number in array .py
@@ -1,10 +1,10 @@
 def missing(a):
-    n = len(a)
+    n = len(a) + 1
+    total = n * (n +1) // 2
     sum = 0
-    for i in range(n):
-        sum = sum + a[i]
-    m = n + 1
-    miss = (sum - m)// 2
-    return miss
+    for i in a:
+        sum += i
+    missing = total - sum
+    return missing
 a = [1,2,3,4,6]
 print(missing(a))
